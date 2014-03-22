a5a259598c08f373698ab306e14f62e90b5ae038
Sebastien Mirolo <smirolo@fortylines.com>
2014-03-21 18:42:42 -0700

    clean pylint reports

--- deployutils/__init__.py
+++ deployutils/__init__.py
@@ -1,31 +1,32 @@
-# Copyright (c) 2013, The DjaoDjin Team
-#   All rights reserved.
+# Copyright (c) 2014, Fortylines LLC
+# All rights reserved.
 #
-#   Redistribution and use in source and binary forms, with or without
-#   modification, are permitted provided that the following conditions are met:
-#     * Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above copyright
-#       notice, this list of conditions and the following disclaimer in the
-#       documentation and/or other materials provided with the distribution.
-#     * Neither the name of DjaoDjin nor the names of its contributors may
-#       be used to endorse or promote products derived from this software
-#       without specific prior written permission.
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
 #
-#   THIS SOFTWARE IS PROVIDED BY The DjaoDjin Team ''AS IS'' AND ANY
-#   EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-#   WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-#   DISCLAIMED. IN NO EVENT SHALL The DjaoDjin Team LLC BE LIABLE FOR ANY
-#   DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-#   (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-#   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-#   ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-#   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 """
 Function to load site and credentials config files
 """
+
+__version__ = '0.1dev'
 
 # Read environment variable first
 def load_config(confname, module=None, verbose=False):
@@ -48,11 +49,11 @@ def load_config(confname, module=None, verbose=False):
             line = conffile.readline()
             while line != '':
                 if not line.startswith('#'):
-                    look = re.match('(\w+)\s*=\s*(.*)', line)
+                    look = re.match(r'(\w+)\s*=\s*(.*)', line)
                     if look:
                         if 'LOCALSTATEDIR' in look.group(2):
                             value = look.group(2) \
-                                % { 'LOCALSTATEDIR': module.APP_ROOT + '/var' }
+                                % {'LOCALSTATEDIR': module.APP_ROOT + '/var'}
                         else:
                             value = look.group(2)
                         try:
@@ -69,11 +70,11 @@ def load_config(confname, module=None, verbose=False):
             module.DB_PATH = module.DB_FILENAME
         else:
             module.DB_PATH = module.DB_NAME
-        for pathname in [ module.LOG_FILE ]:
+        for pathname in [module.LOG_FILE]:
             if not os.path.exists(pathname):
                 if not os.path.exists(os.path.dirname(pathname)):
                     os.makedirs(os.path.dirname(pathname))
-                with open(pathname, 'w') as filedesc:
+                with open(pathname, 'w') as _:
                     pass    # touch file
     elif verbose:
         import sys
--- deployutils/management/commands/pullapp.py
+++ deployutils/management/commands/pullapp.py
@@ -1,27 +1,26 @@
-# Copyright (c) 2013, The DjaoDjin Team
-#   All rights reserved.
+# Copyright (c) 2014, Fortylines LLC
+# All rights reserved.
 #
-#   Redistribution and use in source and binary forms, with or without
-#   modification, are permitted provided that the following conditions are met:
-#     * Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above copyright
-#       notice, this list of conditions and the following disclaimer in the
-#       documentation and/or other materials provided with the distribution.
-#     * Neither the name of DjaoDjin nor the names of its contributors may
-#       be used to endorse or promote products derived from this software
-#       without specific prior written permission.
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
 #
-#   THIS SOFTWARE IS PROVIDED BY The DjaoDjin Team ''AS IS'' AND ANY
-#   EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-#   WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-#   DISCLAIMED. IN NO EVENT SHALL The DjaoDjin Team LLC BE LIABLE FOR ANY
-#   DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-#   (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-#   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-#   ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-#   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import datetime, fnmatch, logging, os, subprocess
 
@@ -77,9 +76,9 @@ def fetch_changes(repo_path, up_commit='master'):
         gitexe = 'git'
         os.chdir(repo_path)
         old_sources_timestamp = sources_latest_timestamp('.')
-        shell_command([ gitexe, 'pull' ])
+        shell_command([gitexe, 'pull'])
         last_up_commit = subprocess.check_output(['git', 'rev-parse', 'HEAD'])
-        shell_command([ gitexe, 'checkout', up_commit ])
+        shell_command([gitexe, 'checkout', up_commit])
         up_commit = subprocess.check_output(['git', 'rev-parse', 'HEAD'])
         new_sources_timestamp = sources_latest_timestamp('.')
         if old_sources_timestamp < new_sources_timestamp:
