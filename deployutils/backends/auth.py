1c18c0df163e7c87730986811fc231cd615e2c57
Sebastien Mirolo <smirolo@djaodjin.com>
2016-01-10 13:45:24 -0800

    revert 9aa97 ProxyUserBackend
    User must be seamlessly created in local db for other apps
    to behave properly.

--- deployutils/backends/auth.py
+++ deployutils/backends/auth.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2016, Djaodjin Inc.
+# Copyright (c) 2015, Djaodjin Inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -22,32 +22,8 @@
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+from django.contrib.auth.backends import RemoteUserBackend
 
-from django.contrib.auth import get_user_model
+class ProxyUserBackend(RemoteUserBackend):
 
-
-class ProxyUserBackend(object):
-
-    def __init__(self):
-        self.users = []
-
-    def authenticate(self, remote_user):
-        """
-        The username passed as ``remote_user`` is considered trusted.  This
-        method simply returns the ``User`` object with the given username.
-        """
-        if not remote_user:
-            return
-
-        UserModel = get_user_model() #pylint:disable=invalid-name
-        user = UserModel(id=remote_user, username=remote_user)
-        self.users += [user]
-        user.id = len(self.users) - 1
-        return user
-
-    def get_user(self, user_id):
-        try:
-            return self.users[user_id]
-        except IndexError:
-            return None
-
+    pass
