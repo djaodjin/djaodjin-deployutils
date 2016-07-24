6fb79f620750e147658c18da03aed436024fa5b3
Sebastien Mirolo <smirolo@djaodjin.com>
2016-07-23 22:08:09 -0700

    adds AccoutMixin

---
+++ deployutils/mixins.py
@@ -0,0 +1,88 @@
+# Copyright (c) 2016, DjaoDjin inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from __future__ import unicode_literals
+
+from django.core.exceptions import ImproperlyConfigured
+from django.http import Http404
+from django.utils.translation import ugettext as _
+
+
+class AccountMixin(object):
+    """
+    Mixin to use in views that will retrieve an account object (out of
+    ``account_queryset``) associated to a slug parameter (``account_url_kwarg``)
+    in the URL.
+    The ``account`` property will be ``None`` if either ``account_url_kwarg``
+    is ``None`` or absent from the URL pattern.
+    """
+    account_queryset = None
+    account_lookup_field = None
+    account_url_kwarg = None
+
+    @property
+    def account(self):
+        if not hasattr(self, '_account'):
+            if (self.account_url_kwarg is not None
+                and self.account_url_kwarg in self.kwargs):
+                if self.account_queryset is None:
+                    raise ImproperlyConfigured(
+                        "%(cls)s.account_queryset is None. Define "
+                        "%(cls)s.account_queryset." % {
+                            'cls': self.__class__.__name__
+                        }
+                    )
+                if self.account_lookup_field is None:
+                    raise ImproperlyConfigured(
+                        "%(cls)s.account_lookup_field is None. Define "
+                        "%(cls)s.account_lookup_field as the field used "
+                        "to retrieve accounts in the database." % {
+                            'cls': self.__class__.__name__
+                        }
+                    )
+                kwargs = {'%s__exact' % self.account_lookup_field:
+                    self.kwargs.get(self.account_url_kwarg)}
+                try:
+                    self._account = self.account_queryset.filter(**kwargs).get()
+                except self.account_queryset.model.DoesNotExist:
+                    #pylint: disable=protected-access
+                    raise Http404(_("No %(verbose_name)s found matching"\
+                        "the query") % {'verbose_name':
+                        self.account_queryset.model._meta.verbose_name})
+            else:
+                self._account = None
+        return self._account
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(AccountMixin, self).get_context_data(*args, **kwargs)
+        context.update({'account': self.account})
+        return context
+
+    def get_url_kwargs(self):
+        kwargs = {}
+        for url_kwarg in [self.account_url_kwarg]:
+            url_kwarg_val = self.kwargs.get(url_kwarg, None)
+            if url_kwarg_val:
+                kwargs.update({url_kwarg: url_kwarg_val})
+        return kwargs
