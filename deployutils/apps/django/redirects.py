0f80b4d445a23b0fb964a83175ff1fdd03e4bfaa
Sebastien Mirolo <smirolo@djaodjin.com>
2017-11-21 14:48:59 -0800

    adds redirect_or_denied

--- deployutils/apps/django/redirects.py
+++ deployutils/apps/django/redirects.py
@@ -28,10 +28,51 @@ Helpers to redirect based on session.
 
 from django import http
 from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.core.exceptions import PermissionDenied
 from django.views.generic import RedirectView
 from django.views.generic.base import TemplateResponseMixin
+from django.utils import six
 
 from .mixins import AccessiblesMixin
+
+
+def _get_accept_list(request):
+    http_accept = request.META.get('HTTP_ACCEPT', '*/*')
+    return [item.strip() for item in http_accept.split(',')]
+
+
+def _insert_url(request, redirect_field_name=REDIRECT_FIELD_NAME,
+                inserted_url=None):
+    '''Redirects to the *inserted_url* before going to the orginal
+    request path.'''
+    # This code is pretty much straightforward
+    # from contrib.auth.user_passes_test
+    path = request.build_absolute_uri()
+    # If the login url is the same scheme and net location then just
+    # use the path as the "next" url.
+    login_scheme, login_netloc = six.moves.urllib.parse.urlparse(
+        inserted_url)[:2]
+    current_scheme, current_netloc = six.moves.urllib.parse.urlparse(path)[:2]
+    if ((not login_scheme or login_scheme == current_scheme) and
+        (not login_netloc or login_netloc == current_netloc)):
+        path = request.get_full_path()
+    # As long as *inserted_url* is not None, this call will redirect
+    # anything (i.e. inserted_url), not just the login.
+    from django.contrib.auth.views import redirect_to_login
+    return redirect_to_login(path, inserted_url, redirect_field_name)
+
+
+def redirect_or_denied(request, inserted_url,
+                       redirect_field_name=REDIRECT_FIELD_NAME, descr=None):
+    http_accepts = _get_accept_list(request)
+    if ('text/html' in http_accepts
+        and isinstance(inserted_url, six.string_types)):
+        return _insert_url(request, redirect_field_name=redirect_field_name,
+                           inserted_url=inserted_url)
+    if descr is None:
+        descr = ""
+    raise PermissionDenied(descr)
+
 
 
 class AccountRedirectView(TemplateResponseMixin, AccessiblesMixin,
