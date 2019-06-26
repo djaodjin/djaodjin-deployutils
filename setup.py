83d7d82da7a3bf2505930e2195eddd7d6f3c5e4d
Sebastien Mirolo <smirolo@djaodjin.com>
2019-06-26 15:44:09 -0700

    setuptools creates an error trying to compile some C code

--- setup.py
+++ setup.py
@@ -22,8 +22,8 @@
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#from distutils.core import setup
-from setuptools import setup
+from distutils.core import setup
+#from setuptools import setup
 
 import deployutils
 
