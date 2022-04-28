djaodjin/djaodjin-deployutils

commit d8020972ccba2401d5d18699d429e22a617b3998

filters subscriptions by profiles and ends_at

deployutils/helpers.py
@@ -22,7 +22,7 @@
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import datetime, logging
+import datetime, logging, re
 
 from dateutil.tz import tzlocal
 from pytz import utc
@@ -39,27 +39,39 @@ def as_timestamp(dtime_at=None):
 
 
 def datetime_or_now(dtime_at=None):
+    conv_dtime_at = None
     if not isinstance(dtime_at, datetime.datetime):
         # `datetime.datetime` is a subclass of `datetime.date`.
         if isinstance(dtime_at, six.string_types):
             try:
                 # XXX `parse_datetime`
-                dtime_at = datetime.datetime.strptime(
+                conv_dtime_at = datetime.datetime.strptime(
                     dtime_at, "%Y-%m-%dT%H:%M:%S.%fZ")
             except ValueError as err:
+                pass
+            if not conv_dtime_at:
                 try:
-                    dtime_at = datetime.datetime.strptime(dtime_at, "%Y-%m-%d")
+                    conv_dtime_at = datetime.datetime.strptime(
+                        dtime_at, "%Y-%m-%dT%H:%M:%SZ")
+                except ValueError as err:
+                    pass
+            if not conv_dtime_at:
+                try:
+                    look = re.match(r'(\d\d\d\d-\d\d-\d\d)(.*)', dtime_at)
+                    if look:
+                        conv_dtime_at = datetime.datetime.strptime(
+                            look.group(1), "%Y-%m-%d")
+                        LOGGER.warning("cannot interpret %s", look.group(2))
                 except ValueError as err:
                     LOGGER.warning(err)
-                    dtime_at = None
         elif isinstance(dtime_at, datetime.date):
-            dtime_at = datetime.datetime(
+            conv_dtime_at = datetime.datetime(
                 dtime_at.year, dtime_at.month, dtime_at.day)
-    if not dtime_at:
-        dtime_at = datetime.datetime.utcnow().replace(tzinfo=utc)
-    if dtime_at.tzinfo is None:
-        dtime_at = dtime_at.replace(tzinfo=utc)
-    return dtime_at
+    if not conv_dtime_at:
+        conv_dtime_at = datetime.datetime.utcnow().replace(tzinfo=utc)
+    if conv_dtime_at.tzinfo is None:
+        conv_dtime_at = conv_dtime_at.replace(tzinfo=utc)
+    return conv_dtime_at
 
 
 def full_name_natural_split(full_name):

deployutils/apps/django/mixins/base.py
@@ -76,7 +76,7 @@ class AccessiblesMixin(object):
         return self.redirect_roles
 
     @staticmethod
-    def get_accessible_plans(request):
+    def get_accessible_plans(request, profile=None, at_time=None):
         """
         Returns the list of plans that appear under at least one subscription
         of a profile the `request.user` has a role on.
@@ -85,11 +85,15 @@ class AccessiblesMixin(object):
         for organizations in six.itervalues(request.session.get(
                 'roles', {})):
             for organization in organizations:
-                for subscription in organization.get('subscriptions', []):
-                    plan_key =  subscription.get('plan')
-                    plan = {'slug': plan_key}
-                    if plan_key not in plans:
-                        plans.update({plan_key: plan})
+                if not profile or profile == organization.get('slug'):
+                    for subscription in organization.get('subscriptions', []):
+                        subscription_ends_at = subscription.get('ends_at')
+                        if (not at_time or
+                            at_time < datetime_or_now(subscription_ends_at)):
+                            plan_key =  subscription.get('plan')
+                            plan = {'slug': plan_key}
+                            if plan_key not in plans:
+                                plans.update({plan_key: plan})
         return plans.values()
 
     @staticmethod

commit d967d906ef01c52dfcc4184ca08874fe45f3532f

fixes bug introduced in commit d8020972
To simplify the conditional logic, I introduced the `conv_dtime_at`
variable but never set it to `dtime_at` when it is a datetime object.
As a result passing a datetime object would always result in getting
the current time back.

deployutils/helpers.py
@@ -40,8 +40,10 @@ def as_timestamp(dtime_at=None):
 
 def datetime_or_now(dtime_at=None):
     conv_dtime_at = None
-    if not isinstance(dtime_at, datetime.datetime):
+    if isinstance(dtime_at, datetime.datetime):
         # `datetime.datetime` is a subclass of `datetime.date`.
+        conv_dtime_at = dtime_at
+    if not conv_dtime_at:
         if isinstance(dtime_at, six.string_types):
             try:
                 # XXX `parse_datetime`